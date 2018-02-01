71f6441db8c1498e7f2810d68a2cba5289aff986
charlesdrews <charlesdrews@gmail.com>
2018-02-01 14:30:05 -0500

    Add notification feature to background job
    The MtaAlertJobService launches a notification showing the data it
    just retrieved. Also, the JobScheduler was updated to use a combo of
    setMinimumLatency() and setOverrideDeadline() rather than setPeriodic()
    because the latter does not allow any delay before the initial execution
    of a periodic job, and we don't want a notification to launch right away
    after the app runs.

--- app/src/main/java/nyc/c4q/unit5finalassessment/android/MainActivity.java
+++ app/src/main/java/nyc/c4q/unit5finalassessment/android/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int MTA_ALERT_JOB_ID = 123;
     private static final int JOB_INTERVAL = 30 * 60 * 1000; // 30 minutes in milliseconds
+    private static final int JOB_INTERVAL_MAX = 60 * 60 * 1000; // 60 minutes in milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
         JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
 
+        // I decided not to use setPeriodic(JOB_INTERVAL) because there's no way to delay
+        // the initial execution of a periodic job, and I don't want a notification to launch
+        // right away when the app is opened. Instead, I set the minimum latency (delay) to be
+        // JOB_INTERVAL, and then in the JobService when the work is done and jobFinished() is
+        // called, I pass in true for the needsReschedule parameter so it reschedules.
         JobInfo jobInfo = new JobInfo.Builder(MTA_ALERT_JOB_ID,
                 new ComponentName(this, MtaAlertJobService.class))
-                .setPeriodic(JOB_INTERVAL)
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setPersisted(true)
+                .setMinimumLatency(JOB_INTERVAL)
+                .setOverrideDeadline(JOB_INTERVAL_MAX)
                 .build();
 
         if (jobScheduler != null) {
--- app/src/main/java/nyc/c4q/unit5finalassessment/android/MtaAlertJobService.java
+++ app/src/main/java/nyc/c4q/unit5finalassessment/android/MtaAlertJobService.java
@@ -1,11 +1,19 @@
 package nyc.c4q.unit5finalassessment.android;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.List;
 
+import nyc.c4q.unit5finalassessment.R;
 import nyc.c4q.unit5finalassessment.model.LineStatus;
 import nyc.c4q.unit5finalassessment.services.MtaStatusProvider;
 import nyc.c4q.unit5finalassessment.services.MtaStatusProviderDbFirst;
@@ -24,6 +32,11 @@ import nyc.c4q.unit5finalassessment.services.network.SimpleMtaStatusNetworkSvc;
 public class MtaAlertJobService extends JobService {
 
     private static final String TAG = "MtaAlertJobService";
+    private static final String NOTIFICATION_CHANNEL = "MTA_Status_Notifications";
+    private static final int NOTIFICATION_ID = 23456;
+    private static final String NOTIFICATION_TITLE = "MTA Subway Status Alerts";
+    private static final String NOTIFICATION_TEXT = "Current alerts:";
+    private static final String DATE_FORMAT_PATTERN = "M/d/yy h:mma";
 
     @Override
     public boolean onStartJob(final JobParameters jobParameters) {
@@ -41,11 +54,11 @@ public class MtaAlertJobService extends JobService {
                         for (LineStatus lineStatus : lineStatuses) {
                             Log.d(TAG, "onMtaStatusesReady: " + lineStatus.getName() + " " +
                                     lineStatus.getStatus() + " " +
-                                    lineStatus.getFormattedDateTime("M/d/yy h:mma"));
+                                    lineStatus.getFormattedDateTime(DATE_FORMAT_PATTERN));
 
                             launchNotification(lineStatuses);
 
-                            jobFinished(jobParameters, false);
+                            jobFinished(jobParameters, true);
                         }
                     }
                 });
@@ -58,6 +71,50 @@ public class MtaAlertJobService extends JobService {
     }
 
     private void launchNotification(List<LineStatus> lineStatuses) {
+        NotificationManager manager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+        if (manager == null) {
+            Log.d(TAG, "launchNotification: Unable to retrieve Notification System Service");
+            return;
+        }
+
+        // If API is 26 or above, must set up a notification channel
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            manager.createNotificationChannel(new NotificationChannel(
+                    NOTIFICATION_CHANNEL, NOTIFICATION_CHANNEL,
+                    NotificationManager.IMPORTANCE_DEFAULT));
+        }
+
+        // Add MTA service status data to an InboxStyle object to show a list in the notification
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        for (LineStatus lineStatus : lineStatuses) {
+            if (!lineStatus.hasGoodService()) {
+                inboxStyle.addLine(lineStatus.getName() + " " +
+                        lineStatus.getStatus() + " " +
+                        lineStatus.getFormattedDateTime(DATE_FORMAT_PATTERN));
+            }
+        }
+
+        // Create a pending intent so the MainActivity launches when notification is tapped
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                (int) System.currentTimeMillis(),
+                new Intent(this, MainActivity.class),
+                PendingIntent.FLAG_CANCEL_CURRENT
+        );
+
+        // Build the actual notification
+        Notification notification = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL)
+                .setSmallIcon(R.drawable.ic_launcher_foreground) //TODO replace w/ real icon
+                .setContentTitle(NOTIFICATION_TITLE)
+                .setContentText(NOTIFICATION_TEXT)
+                .setStyle(inboxStyle)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+
+        // Finally, display the notification
+        manager.notify(NOTIFICATION_ID, notification);
     }
 }
